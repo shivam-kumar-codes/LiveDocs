71ec60265ed974e46ba856bc38c29d93efb3e29a
shivam-kumar-codes <sk9313589721@gmail.com>
2024-08-24 14:26:45 +0530

    first commit

---
+++ app/(root)/documents/[id]/page.tsx
@@ -0,0 +1,45 @@
+import CollabarativeRoom from "@/components/CollabarativeRoom"
+import { getDocument } from "@/lib/actions/room.actions";
+import { getClerkUsers } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation";
+
+
+const Document = async ({params: {id}} : SearchParamProps) => {
+  const clerkUser = await currentUser();
+  if(!clerkUser)  redirect('/sign-in');
+
+  const room = await getDocument({
+    roomId: id,
+    userId: clerkUser.emailAddresses[0].emailAddress,
+  });
+
+  if(!room) redirect('/');
+
+  //TODO: assess the permissions of the user to access the document
+  const userIds = Object.keys(room.usersAccesses);
+  const users = await getClerkUsers({userIds});
+
+  const usersData = users.map((user: User) => ({
+    ...user,
+    userType: room.usersAccesses[user.email]?.includes('room:write')
+    ? 'editor'
+    : 'viewer'
+  }))
+
+  const currentUserType = room.usersAccesses[clerkUser.emailAddresses[0].emailAddress]?.includes
+  ('room:write') ? 'editor' : 'viewer';
+
+  return (
+   <main className="flex w-full flex-col">
+      <CollabarativeRoom
+      roomId = {id}
+      roomMetadata = {room.metadata}
+      users = {usersData}
+      currentUserType={currentUserType}
+      />
+   </main>
+  )
+}
+
+export default Document
---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://125973d0433b3ac9826fcea8cc54beee@o4507827021348864.ingest.us.sentry.io/4507827048480768",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
